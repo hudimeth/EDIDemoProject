844dd24e24f71981b228e0f03744da188dc7a20e
Hudi Meth <methyehudis@gmail.com>
2024-11-11 10:12:35 +0200

    added user logic

--- EDIConverterWeb.Data/Migrations/20241111080142_Initial.Designer.cs
+++ EDIConverterWeb.Data/Migrations/20241111080142_Initial.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EDIConverterWeb.Data.Migrations
 {
     [DbContext(typeof(EDIDbContext))]
-    [Migration("20240912084247_Initial")]
+    [Migration("20241111080142_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
